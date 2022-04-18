991516c4137767670fde1284f7793c93600423c8
Jonathan Kevin <jonathan.barrence-terceiro@cea.com.br>
2022-04-18 12:40:35 -0300

    curso pronto

--- cypress/integration/hardcoded-assertion/sample.spec.js
+++ cypress/integration/hardcoded-assertion/sample.spec.js
@@ -11,17 +11,17 @@ describe('Hardcoded assertion bad practice', () => {
   })
 
   it('searches', () => {
+    const { hits } = require('../../fixtures/stories')
     cy.search('cypress.io')
     cy.wait('@getStories')
 
     cy.get('.table-row')
       .as('tableRows')
-      .should('have.length', 2)
-    cy.get('@tableRows')
-      .eq(0)
-      .should('contain', 'Agile Testing')
-    cy.get('@tableRows')
-      .eq(1)
-      .should('contain', 'Clean Code')
+      .should('have.length', hits.length)
+    hits.forEach((hits, index) => {
+      cy.get('@tableRows')
+      .eq(index)
+      .should('contain', hits.title)
+    })
   })
 })
